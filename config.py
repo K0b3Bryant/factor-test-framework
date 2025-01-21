8abae1b8780a735c94333b324197c9759fe4a979
Marco <sejereje1011@gmail.com>
2025-01-21 10:57:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ CONFIG = {
     'date_format': '%Y-%m-%d',
     'p_value_threshold': 0.05,
     'return_type': 'log',  # Options: 'log' or 'simple'
+    'lasso_alpha': 0.01,
+    'ridge_alpha': 0.01
 }
